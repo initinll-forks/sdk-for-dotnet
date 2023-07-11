b2c7917f16720ca48a06c4f7cfde2d4ee648c609
initinll <initinli@yahoo.com>
2023-07-11 21:19:21 +0530

    added middleware AppwriteExceptionHandler to send AppwriteException in case of status code>400

---
+++ src/Appwrite.Core/Helpers/AppwriteExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace Appwrite.Core.Helpers;
+
+internal class AppwriteExceptionHandler : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var response = await base.SendAsync(request, cancellationToken);
+        var statusCode = (int)response.StatusCode;
+
+        if (statusCode >= 400)
+        {
+            var contentType = response.Content.Headers
+                .GetValues("Content-Type")
+                .FirstOrDefault() ?? string.Empty;
+
+            var isJson = contentType.Contains("application/json");
+            var message = await response.Content.ReadAsStringAsync();
+
+            if (isJson)
+            {
+                message = JsonDocument
+                    .Parse(message)
+                    .RootElement
+                    .GetProperty("message")
+                    .GetString();
+            }
+
+            throw new AppwriteException(message, statusCode);
+        }
+
+        return response;
+    }
+}
