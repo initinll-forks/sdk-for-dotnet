f1f12de98141edb5602ca42cf8e22fb3aee5d23b
initinll <initinli@yahoo.com>
2023-07-11 21:37:53 +0530

    added test client

---
+++ src/Appwrite.Core.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using Appwrite.Core;
+using Appwrite.Core.Services;
+
+var endpoint = String.Empty;
+var project = String.Empty;
+var apikey = String.Empty;
+
+var client = new Client()
+    .SetEndpoint(endpoint)
+    .SetProject(project)
+    .SetKey(apikey);
+try
+{
+    var account = new Account(client);
+    var test = await account.UpdatePassword("test");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception - {ex.Message}");
+}
