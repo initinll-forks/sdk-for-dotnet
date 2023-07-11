4459f66d792e31992261484e3e11fe264189deea
initinll <initinli@yahoo.com>
2023-07-11 22:23:13 +0530

    removed optional parameters

--- src/Appwrite.Core/Apis/IUsers.cs
+++ src/Appwrite.Core/Apis/IUsers.cs
@@ -7,31 +7,31 @@ namespace Appwrite.Core.Apis;
 internal interface IUsers
 {
     [Post("/users")]
-    Task<User> Create([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters = null, CancellationToken cancellationToken = default);
+    Task<User> Create([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters, CancellationToken cancellationToken = default);
 
     [Post("/users/bcrypt")]
-    Task<User> CreateBcryptUser([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters = null, CancellationToken cancellationToken = default);
+    Task<User> CreateBcryptUser([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters, CancellationToken cancellationToken = default);
 
     [Post("/users/md5")]
-    Task<User> CreateMD5User([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters = null, CancellationToken cancellationToken = default);
+    Task<User> CreateMD5User([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters, CancellationToken cancellationToken = default);
 
     [Post("/users/argon2")]
-    Task<User> CreateArgon2User([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters = null, CancellationToken cancellationToken = default);
+    Task<User> CreateArgon2User([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters, CancellationToken cancellationToken = default);
 
     [Post("/users/sha")]
-    Task<User> CreateSHAUser([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters = null, CancellationToken cancellationToken = default);
+    Task<User> CreateSHAUser([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters, CancellationToken cancellationToken = default);
 
     [Post("/users/phpass")]
-    Task<User> CreatePHPassUser([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters = null, CancellationToken cancellationToken = default);
+    Task<User> CreatePHPassUser([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters, CancellationToken cancellationToken = default);
 
     [Post("/users/scrypt")]
-    Task<User> CreateScryptUser([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters = null, CancellationToken cancellationToken = default);
+    Task<User> CreateScryptUser([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters, CancellationToken cancellationToken = default);
 
     [Post("/users/scrypt-modified")]
-    Task<User> CreateScryptModifiedUser([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters = null, CancellationToken cancellationToken = default);
+    Task<User> CreateScryptModifiedUser([Body(BodySerializationMethod.UrlEncoded)] IDictionary<string, object> bodyParameters, CancellationToken cancellationToken = default);
 
     [Get("/users")]
-    Task<UserList> List([Query] IDictionary<string, object> queryParameters = null, CancellationToken cancellationToken = default);
+    Task<UserList> List([Query] IDictionary<string, object> queryParameters, CancellationToken cancellationToken = default);
 
     [Get("/users/{userId}")]
     Task<User> Get(string userId, CancellationToken cancellationToken = default);
